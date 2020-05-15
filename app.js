4b22ab6fac24d44a70578a5138ea665629f50b24
urosgluscevic <56654298+urosgluscevic@users.noreply.github.com>
2020-05-15 13:46:44 +0200

    removed unneccessery packages (we only need puppeteer), set the basis for the acctual scraping

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-const axios = require("axios");
-const cheerio = require("cheerio");
 const puppeteer = require("puppeteer");
 
 (async () => {  //iife, async function ("await" needed everywhere)
@@ -9,8 +7,20 @@ const puppeteer = require("puppeteer");
     await page.goto("https://www.realitica.com/apartmani/Crna-Gora/");  //navigates to the page
     await page.click("input[value='']"); //deselects "sve" option
     await page.click("input[value='Apartment']") // selects "Apartments"
-    await page.click("button[type='submit']") // submits the form
+
+    await Promise.all([
+        page.waitForNavigation(), //waits for the url to change
+        await page.click("button[type='submit']") // submits the form
+    ])
+
     await page.screenshot({path: "example.png"}); // takes a screenshot, for testing
+
+    const result = await page.evaluate(() => { //page.evaluate() lets you run functions in the context of the browser (therefore allows you to do query selectors and interact with DOM a bit)
+        let div = document.querySelector("#left_column_holder > div:nth-child(11) > div:nth-child(3)").innerText; //this is just the selector for the first article. we will probably have to use a loop to get all the elements because none of the elements have any classes or ids
+        return div
+    })
+
+    console.log(result)
 
     console.log("done");
 
