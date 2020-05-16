afb077bb84ed5a2e126ea2037bf4e3cee832c3fd
urosgluscevic <56654298+urosgluscevic@users.noreply.github.com>
2020-05-16 23:18:30 +0200

    parsed the result

--- app.js
+++ app.js
@@ -20,20 +20,21 @@ const puppeteer = require("puppeteer");
         page.click("#left_column_holder > div:nth-child(11) > div:nth-child(3) > a:nth-child(9)") // gets the data for an article
     ])
 
-    // await page.screenshot({path: "example.png"}); // takes a screenshot, for testing
-
     const result = await page.evaluate(()=>{
-        // const data = document.querySelectorAll("strong"); //this manages to get only the titles, not their respective values (the values are not placed inside separate elements, rather just inside the main div, so we cannot pull them out individually, see the below approach)
-        // const dataArray = Array.from(data).map((elem)=> $(elem).text());
-        // return dataArray
-
         const data = document.querySelector("#listing_body").innerText; // we can parse this string using data.split(":")
         return data
     })
 
-    console.log(result)
+    // console.log(result)
 
-    console.log("done");
+    const split = result.split(":"); //beginns the parsing proccess
+
+    const parsed = [];
+    split.forEach((elem)=>{
+        parsed.push(elem.slice(0, elem.indexOf("\n"))) //parses the result
+    })
+
+    console.log(parsed)
 
     await browser.close() //closes the browser (it can't be accessed anymore)
 })()
