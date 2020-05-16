ab2b483c3be2073494fa8273c18cf20054c16f3f
urosgluscevic <56654298+urosgluscevic@users.noreply.github.com>
2020-05-16 16:32:12 +0200

    pulled out all the data as a string

--- app.js
+++ app.js
@@ -17,12 +17,21 @@ const puppeteer = require("puppeteer");
 
     await Promise.all([
         page.waitForNavigation(), //waits for the url to change
-        page.click("#left_column_holder > div:nth-child(11) > div:nth-child(3) > a:nth-child(9)") // submits the form
+        page.click("#left_column_holder > div:nth-child(11) > div:nth-child(3) > a:nth-child(9)") // gets the data for an article
     ])
 
-    await page.screenshot({path: "example.png"}); // takes a screenshot, for testing
+    // await page.screenshot({path: "example.png"}); // takes a screenshot, for testing
 
-    console.log(page.url()) //the url, might use it to scrape the page with cheerio because it is easier
+    const result = await page.evaluate(()=>{
+        // const data = document.querySelectorAll("strong"); //this manages to get only the titles, not their respective values (the values are not placed inside separate elements, rather just inside the main div, so we cannot pull them out individually, see the below approach)
+        // const dataArray = Array.from(data).map((elem)=> $(elem).text());
+        // return dataArray
+
+        const data = document.querySelector("#listing_body").innerText; // we can parse this string using data.split(":")
+        return data
+    })
+
+    console.log(result)
 
     console.log("done");
 
